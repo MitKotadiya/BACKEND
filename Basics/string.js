d78ca87d72e35b3c4bd53b008343340d8e143339
MitKotadiya <kotadiyamit17552@gmail.com>
2025-07-07 21:49:14 +0530

    add

---
+++ Basics/string.js
@@ -0,0 +1,42 @@
+// string ---> string is collection character
+
+// data = "JavaScript, often abbreviated as JS, is a programming language and core technology of the World Wide Web,"
+// data = 'JavaScript, often abbreviated as JS, is "a programming language" and core technology of the World Wide Web'
+// data = `'JavaScript, often 'abbreviated' as JS, is "a programming language" and core technology of the World Wide Web'`
+
+// console.log(data);
+
+
+let data = "JavaScript, often abbreviated as JS, is a programming language and core technology of the World Wide Web,"
+let num = "6886480000423"
+let space = "         js          "
+
+// console.log(data.toUpperCase());
+// console.log(data.toLowerCase());
+
+// console.log(data.split(","));
+
+// console.log(data.slice(2 , 15));
+
+// console.log(data.at(2));
+
+// console.log(num.padStart(20 , "X"))
+
+// console.log(num.padEnd(15 , "X"))
+
+// console.log(space.trim());
+// console.log(space.trimStart());
+// console.log(space.trimEnd());
+
+// console.log(data.concat(num));
+
+// console.log(num.repeat(2));
+
+// console.log(space.replace("js","html"));
+
+
+
+
+
+
+
